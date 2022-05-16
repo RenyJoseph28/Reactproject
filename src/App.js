8f014a10b50bdc1a193af257e0d68a1cc10963c8
RenyJoseph28 <ajaydvrj@gmail.com>
2022-05-16 14:58:03 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Student from './component/Student';
 function App() {
   return (
     <>
-    <Student/>
+    
     
     </>
 
---
+++ src/component/Counter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const counter = () => {
+  return (
+    <div>counterefrgrf</div>
+  )
+}
+
+export default counter
